f85d79f3eb957eb02a064f5bfacc1f86f6ba21c8
birukshegute <birukshegute@gmail.com>
2024-01-19 15:47:07 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 # Installs flask from pip3 using puppet
 
-include python::flask
-
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure          => installed,
+  provider        => 'pip3',
 }
