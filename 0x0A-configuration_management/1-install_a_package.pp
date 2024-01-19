7e59fa7b4c95c6c2dffcb6a936b92091f54f00f0
birukshegute <birukshegute@gmail.com>
2024-01-19 15:27:40 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Installs flask from pip3 using puppet
 
-exec { 'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+exec { 'Flask':
+  command => '/usr/bin/apt-get -y install Flask -v 2.1.0',
 }
