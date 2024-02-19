64b0d064e391f1681d9f10a2aef8ede67e7c8e0c
birukshegute <birukshegute@gmail.com>
2024-02-19 15:40:45 -0800

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,34 +2,22 @@
 """ Records all tasks that are owned by this employee in CSV format.
 Format: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE """
 
+import csv
 import requests
 from sys import argv
-import csv
 
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
-    response = requests.get(url+'todos/')
-    data = response.json()
-
-    task = []
-    response_2 = requests.get(url+'users/')
-    data_2 = response_2.json()
-
-    for i in data_2:
-        if i['id'] == int(argv[1]):
-            Employee = i['username']
-
-    with open(argv[1] + '.csv', 'w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-
-    for i in data:
-
-        task = []
-        if i['userId'] == int(argv[1]):
-            task.append(i['userId'])
-            task.append(Employee)
-            task.append(i['completed'])
-            task.append(i['title'])
-
-            writer.writerow(task)
+    Id = argv[1]
+    response = requests.get(url+"users/{}".format(Id))
+    username = response.json().get('username')
+    if username is not None:
+        tasks = requests.get(url+"users/{}/todos".format(Id)).json()
+    with open("{}.csv".format(Id), 'w', newline='') as csvfile:
+        writeFile = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writeFile.writerow([int(Id),
+                                   username,
+                                   task.get('completed'),
+                                   task.get('title')])
