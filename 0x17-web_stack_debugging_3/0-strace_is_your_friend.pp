2ee89f39b2f84906b9de10ab75b46d1d59e4906f
birukshegute <birukshegute@gmail.com>
2024-03-06 13:04:24 -0800

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
-# To the LD_LIBRARY_PATH, we add libfoo.so path
-file { ‘/etc/profile.d/custom_ld_library_path.sh’:
-ensure => present,
-content => ‘export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/path/to/libfoo’,
-mode => ‘0644’,
-}
-# Restart Apache
-service { ‘apache2’:
-ensure => running,
-subscribe => File[‘/etc/profile.d/custom_ld_library_path.sh’],
+# updates wrong `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'update-phpp-to-php':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
