5850a2b9843f3ee9c93c8b6feb86d8b63f7e8c8a
birukshegute <birukshegute@gmail.com>
2024-03-07 23:35:20 -0800

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,16 +6,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """function queries the Reddit API and returns the number of subscribers"""
-    url = ("https://www.reddit.com/dev/api/{}/about.json"
-           .format(subreddit))
-    headers = {"User-Agent": "CustomClient/1.0"}
-    info = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/dev/api.json".format(subreddit)
+    headers = {"User-Agent": "Headers"}
+    info = requests.get(url, headers=headers)
 
-    if info.status_code != 200:
+    if info.status_code != 200: 
         return (0)
     info = info.json()
-    if 'data' in info:
-        return (info.json().get("data").get("subscribers"))
-
-    else:
-        return (0)
+    return (info.json().get("data").get("subscribers"))
