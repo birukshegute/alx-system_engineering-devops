36a96a7339dde9ce01fc69601a1f11099e82e2d5
birukshegute <birukshegute@gmail.com>
2024-03-13 12:56:32 -0700

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# fix our stack so that we get to 0 failed requests.
+exec { 'fix-`our-stack':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+exec { 'restart-nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
