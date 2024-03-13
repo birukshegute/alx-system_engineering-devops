17ddfa30f4e44fa8bdd747b61fafff2b7626c923
birukshegute <birukshegute@gmail.com>
2024-03-13 13:02:03 -0700

    task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# Change the OS configuration to login with the holberton user and open a file 
+exec { 'increase-file-limit-1':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+exec { 'increase-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
