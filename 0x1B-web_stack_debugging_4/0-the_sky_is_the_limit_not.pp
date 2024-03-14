3ad542a13b39ff24d5d2371e84ccef199dbef71c
birukshegute <birukshegute@gmail.com>
2024-03-14 14:11:10 -0700

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 exec { 'fix-`our-stack':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
+}
 
 exec { 'restart-nginx':
   command => 'nginx restart',
